d6a5bb9e20a6d7f72498520fbfd969d405948667
washucode <estherw8525@gmail.com>
2023-11-06 01:19:34 +0300

    task 4

---
+++ web_dynamic/4-hbnb.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
+from flask import Flask, render_template
+import uuid
+app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+def sort_by_name(obj):
+    return obj.name
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/4-hbnb/', strict_slashes=False)
+def hbnb():
+    """HBNB filters to dynamic from static !"""
+    cache_id = str(uuid.uuid4())
+    states_obj = sorted(storage.all(State).values(), key=sort_by_name)
+    state_city_obj = []
+    for state in states_obj:
+        cities = sorted(state.cities, key=sort_by_name)
+        state_city_obj.append([state, cities])
+    amenities = sorted(storage.all(Amenity).values(), key=sort_by_name)
+    places = sorted(storage.all(Place).values(), key=sort_by_name)
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+
+    return render_template('4-hbnb.html', states=state_city_obj,
+                           amenities=amenities, places=places, users=users,
+                           cache_id=cache_id)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
