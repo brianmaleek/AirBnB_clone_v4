6f87b7cf65575ae76fca0939c957d4da98865cca
washucode <estherw8525@gmail.com>
2023-11-06 00:47:07 +0300

    task 3

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,22 @@
 $('document').ready(function () {
   const amenityDict = {};
   $('input[type="checkbox"]').change(function () {
-    if ($(this).is( ":checked" )) {
+    if ($(this).is(':checked')) {
       amenityDict[$(this).data('id')] = $(this).data('name');
     } else {
-        delete amenityDict[$(this).data('id')];
+      delete amenityDict[$(this).data('id')];
     }
     $('.amenities h4').text(Object.values(amenityDict).join(', '));
-    });
+  });
 
   // 2. Function update API status
   // File: web_dynamic/static/scripts/2-hbnb.js
   function updateStatus () {
-    $.get("http://0.0.0.0:5000/api/v1/status/", function (data) {
-      if (data.status === "OK") {
-        $("#api_status").addClass("available");
+    $.get('http://0.0.0.0:5000/api/v1/status/', function (data) {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
       } else {
-        $("#api_status").removeClass("available");
+        $('#api_status').removeClass('available');
       }
     });
   }
---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,47 @@
+$('document').ready(function () {
+  const amenityDict = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
+});
+
+$.ajax({
+  url: 'http:///0.0.0.0:5001/api/v1/status/',
+  type: 'GET',
+  dataType: 'json',
+  success: function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  },
+  error: function (error) {
+    console.log(error);
+  }
+});
+
+$.ajax({
+  url: 'http://http://0.0.0.0:5001/api/v1/places_search',
+  type: 'POST',
+  data: '{}',
+  dataType: 'json',
+  contentType: 'application/json',
+  success: function (data) {
+    for (const place of data) {
+      $('.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night"><p>' + place.price_by_night +
+            '</p></div></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest +
+            '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' +
+            place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' +
+            place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+    }
+  },
+  error: function (error) {
+    console.log(error);
+  }
+});
