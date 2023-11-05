e4202f93c7131be88cdef0588110fd16da30c43b
brianmaleek <maleekmalik@gmail.com>
2023-11-05 11:51:05 +0300

    1-hbnb.js update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -8,4 +8,18 @@ $('document').ready(function () {
     }
     $('.amenities h4').text(Object.values(amenityDict).join(', '));
     });
+
+  // 2. Function update API status
+  // File: web_dynamic/static/scripts/2-hbnb.js
+  function updateStatus () {
+    $.get("http://0.0.0.0:5000/api/v1/status/", function (data) {
+      if (data.status === "OK") {
+        $("#api_status").addClass("available");
+      } else {
+        $("#api_status").removeClass("available");
+      }
+    });
+  }
+  // call updateStatus function when page loads
+  updateStatus();
 });
